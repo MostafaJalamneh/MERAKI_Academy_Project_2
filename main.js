1734b9fe4741336620c4a3f3da13698ab907ca79
Your Name <you@example.com>
2021-04-27 15:09:08 +0300

    First design for product section

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ const input=$("#search")
 const nav=$("#divnav")
 const eleHead=$("#eleHead")
 const cloHead=$("#cloHead")
-
+const decHead=$("#decHead")
+const bagHead=$("#bagHead")
+const kitHead=$("#kitHead")
 
 
 header.text("E-Commerce Website")
 eleHead.text("Electronic")
 cloHead.text("Clothes")
+decHead.text("Decor")
+bagHead.text("Shoes and bags")
+kitHead.text("kitchen")
 
 
 divnav.appendTo(body)
