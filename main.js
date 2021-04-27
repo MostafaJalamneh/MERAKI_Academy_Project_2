4e6dde7cee2ed8f5151eed662aff5da9ddd110ab
Your Name <you@example.com>
2021-04-27 13:20:35 +0300

    Start create sectionss box

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ const body=$("body");
 const header=$("#header");
 const input=$("#search")
 const nav=$("#divnav")
+const eleHead=$("#eleHead")
+const cloHead=$("#cloHead")
+
+
 
 header.text("E-Commerce Website")
+eleHead.text("Electronic")
+cloHead.text("Clothes")
 
 
 divnav.appendTo(body)
