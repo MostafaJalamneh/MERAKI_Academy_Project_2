089339d10762554884c5cd09fe5ee3c64b5ef170
Your Name <you@example.com>
2021-04-28 12:03:48 +0300

    Finish first design for Sub section

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const input = $("#search")
 const nav = $("#divnav")
 const divmain = $("#divmain")
 const divP = $("#divP")
+const divShB = $("#divShB")
+const divSho = $("#divSho")
+
+
+
 
 const subMain = $("#subMain")
 const eleHead = $("#eleHead")
@@ -17,21 +22,9 @@ const divClo = $("#divClo")
 const divDeco = $("#divDeco")
 const divDe = $("#divDe")
 
-
-
-
-
-
-
-
-
 const elecTa = $("#elecTa")
 const td1 = $("#td1")
 const divElec = $("#divElec")
-
-
-
-
 
 header.css("color", "#CCCCCC");
 td1.css("background-image", "url(./images/HP.png)")
@@ -45,10 +38,12 @@ kitHead.text("kitchen")
 divElTa.hide();
 divClot.hide()
 divDeco.hide()
+divShB.hide()
 
 let eCount = 0;
 let cCount = 0;
-let dCount=0
+let dCount = 0
+let sCount=0;
 
 
 nav.appendTo(body)
@@ -56,7 +51,53 @@ divP.appendTo(body)
 divElTa.appendTo(body)
 divClot.appendTo(body)
 divDeco.appendTo(body)
+divShB.appendTo(body)
 
 
+divElec.on("click", () => {
+    eCount++
+    if (eCount % 2 !== 0 || eCount === 0) {
+        divElTa.show();
+        divClot.hide();
+        divDeco.hide();
+        divShB.hide()
+    } else if (eCount % 2 === 0) {
+        divElTa.hide()
+    }
 
+})
+divClo.on("click", () => {
+    cCount++
+    if (cCount % 2 !== 0 || cCount === 0) {
+        divClot.show();
+        divElTa.hide();
+        divDeco.hide();
+        divShB.hide()
+    } else if (cCount % 2 === 0) {
+        divClot.hide()
+    }
 
+})
+divDe.on("click", () => {
+    dCount++
+    if (dCount % 2 !== 0 || dCount === 0) {
+        divDeco.show();
+        divClot.hide();
+        divElTa.hide();
+        divShB.hide()
+    } else if (dCount % 2 === 0) {
+        divDeco.hide()
+    }
+
+})
+divSho.on("click", () => {
+    sCount ++
+    if (sCount % 2 !== 0 || sCount === 0) {
+        divShB.show();
+        divDeco.hide();
+        divClot.hide();
+        divElTa.hide();
+    } else if (sCount % 2 === 0) {
+        divShB.hide()
+    }
+})
