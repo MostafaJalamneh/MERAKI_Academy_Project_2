bea83b0c9024b755d4bb3a6f8e7cb07984c3514b
Your Name <you@example.com>
2021-04-28 11:19:22 +0300

    Create third table

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
-const body=$("body");
-const header=$("#header");
-const input=$("#search")
-const nav=$("#divnav")
-const divmain=$("#divmain")
-const divP=$("#divP")
+const body = $("body");
+const header = $("#header");
+const input = $("#search")
+const nav = $("#divnav")
+const divmain = $("#divmain")
+const divP = $("#divP")
 
-const subMain=$("#subMain")
-const eleHead=$("#eleHead")
-const cloHead=$("#cloHead")
-const decHead=$("#decHead")
-const bagHead=$("#bagHead")
-const kitHead=$("#kitHead")
-const divElTa=$("#divElTa")
-const divClot=$("#divClot")
+const subMain = $("#subMain")
+const eleHead = $("#eleHead")
+const cloHead = $("#cloHead")
+const decHead = $("#decHead")
+const bagHead = $("#bagHead")
+const kitHead = $("#kitHead")
+const divElTa = $("#divElTa")
+const divClot = $("#divClot")
+const divClo = $("#divClo")
+const divDeco = $("#divDeco")
+const divDe = $("#divDe")
 
 
-const elecTa=$("#elecTa")
-const td1=$("#td1")
-const divElec=$("#divElec")
+
+
+
+
+
+
+
+const elecTa = $("#elecTa")
+const td1 = $("#td1")
+const divElec = $("#divElec")
 
 
 
 
 
 header.css("color", "#CCCCCC");
-td1.css("background-image","url(./images/HP.png)")
+td1.css("background-image", "url(./images/HP.png)")
 
 header.text("E-Commerce Website")
 eleHead.text("Electronic")
@@ -33,22 +43,20 @@ decHead.text("Decor")
 bagHead.text("Shoes and bags")
 kitHead.text("kitchen")
 divElTa.hide();
-let a=0
+divClot.hide()
+divDeco.hide()
+
+let eCount = 0;
+let cCount = 0;
+let dCount=0
 
 
 nav.appendTo(body)
 divP.appendTo(body)
 divElTa.appendTo(body)
 divClot.appendTo(body)
+divDeco.appendTo(body)
 
 
-divElec.on("click",()=>{
-    a++
-    if(a%2!==0){
-    divElTa.show();
-    }else if(a%2===0){
-        divElTa.hide()
-    }
-})
 
 
